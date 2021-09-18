3e997f0135bd49abf57f6e2c1aa72848557a9c21
Xie Yuheng <xyheme@gmail.com>
2021-09-18 18:53:35 +0800

    up

--- src/tests/html-inline.test.ts
+++ src/tests/html-inline.test.ts
@@ -19,3 +19,25 @@ a <x /> b
     },
   ])
 }
+
+{
+  const htmlInline = `
+
+a <x> hi </x> b
+
+`
+  const node = parseDocument(htmlInline)
+
+  assertDocument(node, [
+    {
+      kind: "Paragraph",
+      children: [
+        { kind: "Text", value: "a " },
+        { kind: "HtmlInline", value: "<x>" },
+        { kind: "Text", value: " hi " },
+        { kind: "HtmlInline", value: "</x>" },
+        { kind: "Text", value: " b" },
+      ],
+    },
+  ])
+}
