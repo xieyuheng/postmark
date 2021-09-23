9cea5660523d39d846e7e18ffd6f71c9fb2c48e6
Xie Yuheng <xyheme@gmail.com>
2021-09-23 18:27:17 +0800

    up

--- src/code-block-parser/code-block-parser.ts
+++ src/code-block-parser/code-block-parser.ts
@@ -3,6 +3,6 @@ import ty, { Schema } from "@xieyuheng/ty"
 export abstract class CodeBlockParser<A> {
   abstract customKind: string
   abstract schema: Schema<A>
-  abstract accept(info: string): boolean
+  abstract recognize(info: string): boolean
   abstract parse(text: string): A
 }
