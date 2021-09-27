73c5601bff95efdb20b99619bb6c4b37a7455963
Xie Yuheng <xyheme@gmail.com>
2021-09-27 17:30:52 +0800

    - `Parser` -- `parseDocument` also parse `attributes`

--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -1,17 +1,11 @@
 import * as Nodes from "../nodes"
 import * as Commonmark from "../vendor/commonmark"
 import { documentFromCommonmark } from "./document-from-commonmark"
-import { Schema } from "@xieyuheng/ty"
+import ty, { Schema } from "@xieyuheng/ty"
 import frontMatter from "front-matter"
 
 export class Parser {
   commonmarkParser = new Commonmark.Parser()
-
-  parseDocument(text: string): Nodes.Document<undefined> {
-    return documentFromCommonmark(this.commonmarkParser.parse(text), {
-      attributes: undefined,
-    })
-  }
 
   parseDocumentWithFrontMatter<A>(
     text: string,
@@ -22,5 +16,9 @@ export class Parser {
     return documentFromCommonmark(this.commonmarkParser.parse(body), {
       attributes: opts.attributes.validate(attributes),
     })
+  }
+
+  parseDocument(text: string): Nodes.Document<any> {
+    return this.parseDocumentWithFrontMatter(text, { attributes: ty.any() })
   }
 }
