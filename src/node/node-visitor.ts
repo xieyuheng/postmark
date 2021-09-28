ed09ff14ee9690d88361c68721ef0840358a75c8
Xie Yuheng <xyheme@gmail.com>
2021-09-28 16:44:13 +0800

    up

--- src/node/node-visitor.ts
+++ src/node/node-visitor.ts
@@ -2,7 +2,6 @@ import { Node } from "../node"
 import * as Nodes from "../nodes"
 
 export abstract class NodeVisitor<T> {
-  constructor() {}
   default(node: Node): T {
     throw new Error("The default visiting method is not implemented")
   }
